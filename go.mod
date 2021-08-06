02b58aa32ff9ef95d6d62c171dd7ed93043d928e
Ngo SY Long <ngolong192@gmail.com>
2021-08-06 17:53:51 +0900

    init

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module aws-lab.com
 go 1.16
 
 require (
+	github.com/aws/aws-sdk-go v1.40.16 // indirect
 	github.com/aws/aws-sdk-go-v2/config v1.6.0
 	github.com/aws/aws-sdk-go-v2/service/s3 v1.12.0
 )
--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 	"os"
 	"time"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/aws/aws-sdk-go-v2/service/s3/types"
@@ -18,6 +17,11 @@ const (
 	DEFAULT_TIME     = "2006012150405"
 	CONFIG_FILE_NAME = "s3_infor.txt"
 	REGION           = "us-west-2"
+)
+
+var (
+	S3Client   *s3.Client
+	bucketName string
 )
 
 func checkErr(err error) {
@@ -47,24 +51,49 @@ func readFile(fileName string) (string, bool) {
 	return string(content), false
 }
 func createNewConfigFile() {
-	bucketName := fmt.Sprintf("%s-%s", "bucket", time.Now().Format(DEFAULT_TIME))
+	bucketName1 := fmt.Sprintf("%s-%s", "bucket", time.Now().Format(DEFAULT_TIME))
 	f, err := os.OpenFile(CONFIG_FILE_NAME, os.O_APPEND|os.O_WRONLY, 0644)
 	checkErr(err)
-	_, err1 := f.WriteString(bucketName)
+	_, err1 := f.WriteString(bucketName1)
 	checkErr(err1)
 	f.Sync()
 	f.Close()
 }
-
-func main() {
-	bucketName := getConfigFile(CONFIG_FILE_NAME)
+func init() {
+	bucketName = getConfigFile(CONFIG_FILE_NAME)
 	fmt.Printf("\nbkname:%s\n", bucketName)
 	fmt.Printf("%s", bucketName)
 	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(REGION))
 	checkErr(err)
-	S3Client := s3.NewFromConfig(cfg)
-	output, err := S3Client.CreateBucket(context.TODO(), &s3.CreateBucketInput{Bucket: aws.String(bucketName), CreateBucketConfiguration: &types.CreateBucketConfiguration{LocationConstraint: types.BucketLocationConstraintUsWest2}})
+	S3Client = s3.NewFromConfig(cfg)
+}
+func isExitedBucket(bucketName2 string) bool {
+	listBucket, err := S3Client.ListBuckets(context.TODO(), &s3.ListBucketsInput{})
+	checkErr(err)
+	// fmt.Printf("%v", listBucket)
+	fmt.Printf("%v, %v", &listBucket.Buckets[0].Name, *listBucket.Buckets[0].Name)
+	for _, b := range listBucket.Buckets {
+		if *b.Name == bucketName2 {
+			return true
+		}
+	}
+	return false
+}
+func getOrCreateBucket(bucketName3 string) {
+	exited := isExitedBucket(bucketName3)
+	if !exited {
+		_, err := S3Client.CreateBucket(context.TODO(), &s3.CreateBucketInput{Bucket: &bucketName3, CreateBucketConfiguration: &types.CreateBucketConfiguration{LocationConstraint: types.BucketLocationConstraintUsWest2}})
+		checkErr(err)
+	}
+	output, err := S3Client.GetBucketAcl(context.TODO(), &s3.GetBucketAclInput{Bucket: &bucketName3})
 	checkErr(err)
 	fmt.Printf("%s", output)
+}
+func main() {
+	getOrCreateBucket(bucketName)
+
+	// AlcOutput, err := S3Client.GetBucketAcl(context.TODO(), &s3.GetBucketAclInput{Bucket: aws.String(bucketName)})
+	// checkErr(err)
+	// fmt.Printf("%v", AlcOutput)
 
 }
